936b09a2cebc61fb5bfba82b968143d53c9feac6
Isfak-Shakil <isfakshakil@gmail.com>
2020-12-13 23:54:20 +0600

    Tic Tac Toe completed

---
+++ app/src/main/java/com/example/trackyourself/GuessingNumber.java
@@ -0,0 +1,14 @@
+package com.example.trackyourself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuessingNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guessing_number);
+    }
+}
--- app/src/main/java/com/example/trackyourself/MainActivity.java
+++ app/src/main/java/com/example/trackyourself/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    CardView cardView_1,cardView_2,cardView_3,cardView_4,cardView_5,cardView_6;
+    CardView cardView_1,cardView_2,cardView_3,cardView_4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -16,30 +16,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         cardView_1=findViewById(R.id.card_1_Id);
         cardView_2=findViewById(R.id.card_2_Id);
-      //  cardView_3=findViewById(R.id.card_3_Id);
-      //  cardView_4=findViewById(R.id.card_4_Id);
-        cardView_5=findViewById(R.id.card_5_Id);
-        cardView_6=findViewById(R.id.card_6_Id);
+       cardView_3=findViewById(R.id.card_3_Id);
+        cardView_4=findViewById(R.id.card_4_Id);
+
 
         cardView_1.setOnClickListener(this);
         cardView_2.setOnClickListener(this);
         cardView_3.setOnClickListener(this);
         cardView_4.setOnClickListener(this);
-        cardView_5.setOnClickListener(this);
-        cardView_6.setOnClickListener(this);
+
     }
+
 
     @Override
     public void onClick(View v) {
-        if (v.getId()==R.id.card_1_Id){}
+        if (v.getId()==R.id.card_1_Id){
+
+        }
         else if (v.getId()==R.id.card_2_Id){
         }
-      //  else if (v.getId()==R.id.card_3_Id){}
-       // else if (v.getId()==R.id.card_4_Id){}
-        else if(v.getId()==R.id.card_5_Id){
-        //    Intent intent=new Intent(getApplicationContext(),GamesActivity.class);
-          //  startActivity(intent);
+        else if (v.getId()==R.id.card_3_Id){
+                startActivity(new Intent(MainActivity.this,TicTacToe.class));
         }
-        else if(v.getId()==R.id.card_6_Id){}
+        else if (v.getId()==R.id.card_4_Id){
+            startActivity(new Intent(MainActivity.this,GuessingNumber.class));
+        }
     }
 }
